77c4b06a8e3a8281ef9fc0fb60e8b9812b5d020e
Jeyaram <jeyaram5408@gmail.com>
2026-04-01 23:23:41 +0530

    debug register error

--- app/utils/email_service.py
+++ app/utils/email_service.py
@@ -3,18 +3,22 @@ import smtplib
 from email.mime.text import MIMEText
 
 def send_otp_email(to_email, otp):
-    EMAIL = os.getenv("EMAIL")          # your email
-    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # SMTP key
+    # SMTP credentials (Brevo)
+    EMAIL = os.getenv("EMAIL")  # a6dea6001@smtp-brevo.com
+    EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")  # SMTP Key
+
+    # Verified sender email (important)
+    SENDER_EMAIL = os.getenv("SENDER_EMAIL")  # jeyaram5408@gmail.com
 
     msg = MIMEText(f"Your OTP is {otp}")
     msg["Subject"] = "OTP Verification"
-    msg["From"] = EMAIL
+    msg["From"] = SENDER_EMAIL
     msg["To"] = to_email
 
     try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server = smtplib.SMTP("smtp-relay.brevo.com", 587)  # ✅ correct server
         server.starttls()
-        server.login(EMAIL, EMAIL_PASSWORD)
+        server.login(EMAIL, EMAIL_PASSWORD)  # ✅ login with SMTP login
         server.send_message(msg)
         server.quit()
 
