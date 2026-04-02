782ee29a5cdc40a734990f20b42b6d0bd0b1e403
Jeyaram <jeyaram5408@gmail.com>
2026-04-02 14:45:24 +0530

    f

--- app/utils/email_service.py
+++ app/utils/email_service.py
@@ -1,45 +1,21 @@
-import requests
-import os
+import smtplib
+from email.mime.text import MIMEText
 
 def send_otp_email(to_email, otp):
-    API_KEY = os.getenv("BREVO_API_KEY")
-    SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+    sender = "jeyaram5408@gmail.com"
+    app_password = "uvmu utgn szdc wexa"
 
-    if not API_KEY or not SENDER_EMAIL:
-        raise ValueError("Missing BREVO_API_KEY or SENDER_EMAIL")
-
-    url = "https://api.brevo.com/v3/smtp/email"
-
-    payload = {
-        "sender": {
-            "name": "Finance AI App",
-            "email": SENDER_EMAIL
-        },
-        "to": [{"email": to_email}],
-        "subject": "OTP Verification",
-        "htmlContent": f"""
-            <h2>OTP Verification</h2>
-            <p>Your OTP is:</p>
-            <h1>{otp}</h1>
-            <p>This OTP is valid for 5 minutes.</p>
-        """
-    }
-
-    headers = {
-        "accept": "application/json",
-        "api-key": API_KEY,
-        "content-type": "application/json"
-    }
+    msg = MIMEText(f"Your OTP is {otp}")
+    msg["Subject"] = "OTP Verification"
+    msg["From"] = sender
+    msg["To"] = to_email
 
     try:
-        response = requests.post(url, json=payload, headers=headers)
-
-        if response.status_code == 201:
-            return True
-        else:
-            print("FAILED:", response.text)
-            return False
-
+        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+        server.login(sender, app_password)
+        server.send_message(msg)
+        server.quit()
+        return True
     except Exception as e:
         print("EMAIL ERROR:", e)
         return False
